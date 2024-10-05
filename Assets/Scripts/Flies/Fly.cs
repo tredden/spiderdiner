449527faf122feece476db94d33244d3bc3d3f4e
Matthew Soulanille <matthew@soulanille.net>
2024-10-05 13:30:49 -0700

    Make some boids structs

--- Assets/Scripts/Flies/Fly.cs
+++ Assets/Scripts/Flies/Fly.cs
@@ -2,14 +2,45 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public enum Color { UNSET = 0, BLUE = 1, }
+public enum FlyColor { UNSET = 0, BLUE = 1, }
+
+public struct BoidsUpdate {
+    // Separation
+    public float close_dx;
+    public float close_dy;
+
+    // Alignment
+    public float xvel_avg;
+    public float yvel_avg;
+    public int neighboring_boids;
+
+    // Cohesion
+    public float xpos_avg;
+    public float ypos_avg;
+}
+
+public struct FlyData {
+    public float x;
+    public float y;
+    public float vx;
+    public float vy;
+    public FlyColor color;
+    int spiceLevel;
+}
 
 [System.Serializable]
 public struct Fly {
-    public float x;
-    public float y;
-    public float dx;
-    public float dy;
-    public Color color;
-    int spiceLevel;
+    // One of these is the current frame
+    // The other is the previous frame.
+	FlyData[] data = new FlyData[2];
+
+
+
+	public Fly(ref FlyData data) {
+        this.data[0] = data;
+        this.data[1] = data;
+    }
+
+
+
 }    
--- Assets/Scripts/Flies/FlyManager.cs
+++ Assets/Scripts/Flies/FlyManager.cs
@@ -4,12 +4,24 @@ using UnityEngine;
 
 const int MAX_FLIES = 10000;
 
+[System.Serializable]
+struct BoidsRules {
+    public float turnfactor;
+    public float visualRange;
+    public float protectedRange;
+    public float centeringfactor;
+    public float avoidfactor;
+    public float matchingfactor;
+    public float maxspeed;
+    public float minspeed;
+}
+
 public class FlyManager : MonoBehaviour
 {
 	// Active flies
     Fly[] activeFlies = new Fly[MAX_FLIES];
-    // Inactive flies
-    Fly[] inactiveFlies = new Fly[MAX_FLIES];
+
+    BoidsRules boidsRules;
 
     // Start is called before the first frame update
     void Start()
@@ -19,6 +31,9 @@ public class FlyManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Update flies based on boids rules
+
+
         
     }
 
@@ -30,7 +45,5 @@ public class FlyManager : MonoBehaviour
             color = Color.BLUE,
             spiceLevel = 0
         });
-
-
     }
 }
