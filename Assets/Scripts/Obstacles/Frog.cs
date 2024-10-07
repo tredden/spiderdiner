558f87e55bdafa386db762fac4ba7b57f8e8a947
grutledge96 <37202172+grutledge96@users.noreply.github.com>
2024-10-07 03:08:33 -0700

    godfrogger Tongue Anims

--- Assets/Scripts/Obstacles/Frog.cs
+++ Assets/Scripts/Obstacles/Frog.cs
@@ -4,19 +4,51 @@ using UnityEngine;
 
 public class Frog : CircleInfluencer
 {
-    List<FrogTongue> frogTongues = new List<FrogTongue>();
+    enum TongueStatus
+    {
+        MOUTH_CLOSED,
+        MOUTH_OPEN_EARLY,
+        TONGUE_EXTENDING,
+        TONGUE_EXTENDED,
+        TONGUE_RETRACTING,
+        MOUTH_OPEN_LATE
+    }
+
+    [SerializeField]
+    Vector2 pointA;
+    Vector2 pointB;
+    Vector2 pointBTarget;
+    float tongueMoveTime;
+    [SerializeField]
+    List<Vector2> tongueTargets = new List<Vector2>();
     int activeTongue = -1;
 
+    [SerializeField]
+    FrogTongue tongue;
+    [SerializeField]
+    Animator animator;
+
+    [SerializeField]
+    float earlyOpenTime;
     [SerializeField]
     float minTongueTime;
     [SerializeField]
     float maxTongueTime;
     [SerializeField]
-    float minGloatTime;
+    float lateOpenTime;
     [SerializeField]
-    float maxGloatTime;
+    float minClosedTime;
+    [SerializeField]
+    float maxClosedTime;
+
+    [SerializeField]
+    float tongueLeaveSpeed;
+    [SerializeField]
+    float tongueReturnSpeed;
 
     float timeRemaining;
+
+    TongueStatus tongueStatus;
 
     public override void InfluenceFly(ref Fly fly, float dt)
     {
@@ -27,41 +59,77 @@ public class Frog : CircleInfluencer
     protected override void Start()
     {
         base.Start();
-        foreach (FrogTongue ft in GetComponentsInChildren<FrogTongue>()) {
-            frogTongues.Add(ft);
-            ft.gameObject.SetActive(false);
-        }
-        timeRemaining = maxGloatTime;
+        animator.SetBool("isOpen", false);
+        tongue.gameObject.SetActive(false);
+        timeRemaining = maxClosedTime;
     }
 
     // Update is called once per frame
     protected void Update()
     {
         timeRemaining -= Time.deltaTime;
+        if (tongueStatus == TongueStatus.TONGUE_EXTENDING) {
+            pointB = pointA * timeRemaining / tongueMoveTime + pointBTarget * (1f - timeRemaining / tongueMoveTime);
+            tongue.SetPointB(pointB.x, pointB.y);
+        } else if (tongueStatus == TongueStatus.TONGUE_RETRACTING) {
+            pointB = pointBTarget * timeRemaining / tongueMoveTime + pointA * (1f - timeRemaining / tongueMoveTime);
+            tongue.SetPointB(pointB.x, pointB.y);
+        }
         if (timeRemaining <= 0f) {
-            if (activeTongue == -1) {
-                StartTongue();
-            } else {
-                EndTongue();
+            switch (tongueStatus) { // tick to the next state
+                case TongueStatus.MOUTH_CLOSED:
+                    animator.SetBool("isOpen", true);
+                    timeRemaining = earlyOpenTime;
+                    tongueStatus++;
+                    break;
+                case TongueStatus.MOUTH_OPEN_EARLY:
+                    int activeTongue = Random.Range(0, tongueTargets.Count);
+                    pointBTarget = tongueTargets[activeTongue];
+                    pointB = pointA;
+                    tongue.SetPointA(pointA.x, pointA.y);
+                    tongue.SetPointB(pointB.x, pointB.y);
+                    tongue.gameObject.SetActive(true);
+                    tongueMoveTime = (pointBTarget - pointA).magnitude / tongueLeaveSpeed;
+                    timeRemaining = tongueMoveTime;
+                    tongueStatus++;
+                    break;
+                case TongueStatus.TONGUE_EXTENDING:
+                    timeRemaining = Random.Range(minTongueTime, maxTongueTime);
+                    tongueStatus++;
+                    break;
+                case TongueStatus.TONGUE_EXTENDED:
+                    tongueMoveTime = (pointBTarget - pointA).magnitude / tongueReturnSpeed;
+                    timeRemaining = tongueMoveTime;
+                    tongueStatus++;
+                    break;
+                case TongueStatus.TONGUE_RETRACTING:
+                    tongue.gameObject.SetActive(false);
+                    timeRemaining = lateOpenTime;
+                    tongueStatus++;
+                    break;
+                case TongueStatus.MOUTH_OPEN_LATE:
+                    timeRemaining = Random.Range(minClosedTime, maxClosedTime);
+                    animator.SetBool("isOpen", false);
+                    tongueStatus = TongueStatus.MOUTH_CLOSED;
+                    break;
             }
         }
     }
 
-    void StartTongue()
+    public override void OnDrawGizmos()
     {
-        activeTongue = Random.Range(0, frogTongues.Count);
-        frogTongues[activeTongue].gameObject.SetActive(true);
-        // TODO: set anim data
-
-        timeRemaining = Random.Range(minTongueTime, maxTongueTime);
-    }
-
-    void EndTongue()
-    {
-        frogTongues[activeTongue].gameObject.SetActive(false);
-        activeTongue = -1;
-        // TODO: set anim data
-
-        timeRemaining = Random.Range(minGloatTime, maxGloatTime);
+        base.OnDrawGizmos();
+        Gizmos.color = Color.magenta;
+        Vector3 pointA = Vector3.zero;
+        Vector3 pointB = Vector3.zero;
+        pointA.x = this.pointA.x;
+        pointA.y = this.pointA.y;
+        Gizmos.DrawWireSphere(pointA, 1.5f);
+        foreach (Vector2 target in tongueTargets) {
+            pointB.x = target.x;
+            pointB.y = target.y;
+            Gizmos.DrawWireSphere(pointB, 1.5f);
+            Gizmos.DrawLine(pointA, pointB);
+        }
     }
 }
